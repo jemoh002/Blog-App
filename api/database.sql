a5d574cbfc405327992832950287e00520dbfa0d
jemoh002 <jamesnyajuaya@gmail.com>
2024-06-17 16:30:36 +0300

    started working on posts

--- api/database.sql
+++ api/database.sql
@@ -22,5 +22,15 @@ CREATE TABLE posts(
     `uid` INT NOT NULL,
     CONSTRAINT `uid` FOREIGN KEY (`uid`) REFERENCES users(id)
 );
+ALTER TABLE posts ADD COLUMN cat VARCHAR(45);
 
 INSERT INTO posts(`title`, `desc`, `img`, `date`, `uid`) VALUES('test', 'test desc', 'test_url', '2024-07-01', 1);
+INSERT INTO posts(`title`, `desc`, `img`, `date`, `uid`) VALUES(
+"Introducing Gemini: The Next Generation AI", "Google unveils Gemini, a revolutionary AI platform designed to enhance productivity and creativity across various applications.","https://media.istockphoto.com/id/1403500817/photo/the-craggies-in-the-blue-ridge-mountains.jpg?s=612x612&w=0&k=20&c=N-pGA8OClRVDzRfj_9AqANnOaDS3devZWwrQNwZuDSk=", '2024-07-01', 1
+);
+UPDATE posts SET cat='technology' WHERE id=3;
+
+INSERT INTO posts(`title`, `desc`, `img`, `date`, `uid`) VALUES (
+  "Sustainability Initiatives: Google’s Road to Net Zero",
+  "Google outlines its comprehensive sustainability strategy, focusing on achieving net zero emissions by 2030.", "https://images.pexels.com/photos/206359/pexels-photo-206359.jpeg?cs=srgb&dl=pexels-pixabay-206359.jpg&fm=jpg", '2024-06-01', 1
+);
--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,7 +1,18 @@
 import express from "express";
+import {
+  addPost,
+  deletePost,
+  getPost,
+  getPosts,
+  updatePost,
+} from "../controllers/post.js";
 
 const router = express();
 
-router.get("/", (req, res) => {});
+router.get("/", getPosts);
+router.get("/:id", getPost);
+router.post("/", addPost);
+router.delete("/:id", deletePost);
+router.put("/:id", updatePost);
 
 export default router;
